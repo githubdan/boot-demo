344d42adb9d6923badb52cb7b5399dd2dff6ce41
githubdan <dan.r.green@gmail.com>
2021-01-17 18:04:05 -0700

    First push and commit I have no idea what I'm doing.

---
+++ src/test/java/com/example/bootDemo/BootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
